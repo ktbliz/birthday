bc19dfe6d5dd80ad9dc35cc1640a9e9566d913c5
Katie Blizzard <kblizzard11@gmail.com>
2019-04-05 02:04:17 +0000

    structing files

---
+++ bin/run.rb
@@ -0,0 +1,5 @@
+#!/usr/bin/env ruby
+
+require_relative "../config/environment.rb"
+require_relative "../lib/command_line_interface.rb"
+
--- lib/birthday.rb
+++
@@ -1,7 +0,0 @@
-require "birthday/version"
-
-module Birthday
-  class Error < StandardError; end
-  # Your code goes here...
-end
-
