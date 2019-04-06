fb879f02b8bf4255782def6c773d044d44a89662
Katie Blizzard <kblizzard11@gmail.com>
2019-04-06 02:41:44 +0000

    more bithday.rb additions

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -10,6 +10,13 @@ class Birthday
   
   def birthday_format 
     date_array = self.birthday.split("/")
+    month_num = date_array[0]
+    day = date_array[1]
+    year = date_array[2]
+    
+    month_nums = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"] 
+    month_names = ["January", "February", "March", "April", "May", "June", "July", "August", "Septemer", "October", "November", "December"]
+    
   end 
   
   def countdown 
