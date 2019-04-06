a6a6baa817b864ee04a9e65665c2edffc756b301
Katie Blizzard <kblizzard11@gmail.com>
2019-04-06 20:41:37 +0000

    building out birthday_stats.rb

--- lib/birthday_stats.rb
+++ lib/birthday_stats.rb
@@ -5,7 +5,7 @@ require "pry"
 
 class BirthdayStats
   
-  attr_reader :birthday 
+  attr_reader :birthday, :countdown, :turning, :days_alive, :zodiac_sign, :chinese_zodiac, :birthstone 
   
   def initialize(birthday_object)
     @birthday = birthday_object if birthday_object.class == Birthday 
@@ -14,7 +14,13 @@ class BirthdayStats
   def stats_scraper 
     doc = Nokogiri::HTML(open("https://mybirthday.ninja/?m=#{self.birthday.month_name}&d=#{self.birthday.day}&y=#{self.birthday.year}&go=Go"))  
   
-    summary = doc.css("ul.summary")  
+    stats_list = doc.css("ul.summary li").text 
+    @countdown = stats_list[8]
+    @turning = stats_list[9]
+    @days_alive = stats_list[10]
+    @zodiac_sign = stats_list[19]
+    @chinese_zodiac = stats_list[24]
+    @birthstone = stats_list [26]
      
   end 
   
